4e663647dd264552c989fd1bea64ccc2beaf6a04
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-11-25 02:24:57 -0500

    Formating: Formating component

--- src/App.jsx
+++ src/App.jsx
@@ -14,59 +14,59 @@ const CountryList = lazy(() => import("./components/CountryList"));
 const City = lazy(() => import("./components/City"));
 
 const App = () => {
-return (
-<div className="App">
-<Routes>
-<Route index element={<HomePage />} />
-<Route path="pricing" element={<Pricing />} />
-<Route path="product" element={<Product />} />
-<Route path="login" element={<Login />} />
-<Route path="app" element={<AppLayout />}>
-<Route
-index
-element={
-<Suspense fallback={<Spinner />}>
-<Navigate replace to="cities" />
-</Suspense>
-}
-/>
-<Route
-path="cities"
-element={
-<Suspense fallback={<Spinner />}>
-<CityList />
-</Suspense>
-}
-/>
-<Route
-path="cities/:id"
-element={
-<Suspense fallback={<Spinner />}>
-<City />
-</Suspense>
-}
-/>
-<Route
-path="countries"
-element={
-<Suspense fallback={<Spinner />}>
-<CountryList />
-</Suspense>
-}
-/>
-<Route
-path="form"
-element={
-<Suspense fallback={<Spinner />}>
-<Form />
-</Suspense>
-}
-/>
-</Route>
-<Route path="*" element={<PageNotFound />} />
-</Routes>
-</div>
-);
-}
+  return (
+    <div className="App">
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={
+              <Suspense fallback={<Spinner />}>
+                <Navigate replace to="cities" />
+              </Suspense>
+            }
+          />
+          <Route
+            path="cities"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <CityList />
+              </Suspense>
+            }
+          />
+          <Route
+            path="cities/:id"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <City />
+              </Suspense>
+            }
+          />
+          <Route
+            path="countries"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <CountryList />
+              </Suspense>
+            }
+          />
+          <Route
+            path="form"
+            element={
+              <Suspense fallback={<Spinner />}>
+                <Form />
+              </Suspense>
+            }
+          />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
